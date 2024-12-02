4156e1cf39377c8c17cf56541247169bb85372ce
꧁☆*κɪɴɢ*☆꧂ <argumedodie@gmail.com>
2024-12-02 16:54:39 -0500

    Actualizar Grupo•delete.js

--- plugins/Grupo•delete.js
+++ plugins/Grupo•delete.js
@@ -1,27 +1,19 @@
-import { createHash } from 'crypto';  
-import fetch from 'node-fetch';
+let handler = async (m, { conn, usedPrefix, command }) => {
 
-const handler = async (m, { conn, usedPrefix, command }) => {
-
-if (!m.quoted) return conn.reply(m.chat, `🚩 Responde al mensaje que deseas eliminar.`, m);
-
+if (!m.quoted) return conn.reply(m.chat, `🚩 Responde al mensaje que deseas eliminar.`, m, rcanal)
 try {
-let delet = m.message.extendedTextMessage.contextInfo.participant;
-let bang = m.message.extendedTextMessage.contextInfo.stanzaId;
+let delet = m.message.extendedTextMessage.contextInfo.participant
+let bang = m.message.extendedTextMessage.contextInfo.stanzaId
+return conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: null, participant: delet }})
+} catch {
+return conn.sendMessage(m.chat, { delete: m.quoted.vM.key })
+}}
 
-await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet } }, { quoted: null });
-} catch (e) {
-try {
-await conn.sendMessage(m.chat, { delete: m.quoted.vM.key }, { quoted: null });
-} catch (err) {
-return conn.reply(m.chat, `❌ Error al eliminar el mensaje.`, m);
-}}};
+handler.help = ['delete']
+handler.tags = ['grupo']
+handler.command = ['delete', 'del']
+handler.group = false
+handler.admin = true
+handler.botAdmin = true
 
-handler.help = ['delete'];
-handler.tags = ['grupo'];
-handler.command = ['delete', 'del'];
-handler.group = false;
-handler.admin = true;
-handler.botAdmin = true;
-
-export default handler;
+export default handler
