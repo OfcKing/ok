c56843fc1f54036a568c03307925c138b1e47d15
꧁☆*κɪɴɢ*☆꧂ <argumedodie@gmail.com>
2024-12-02 14:50:43 -0500

    𝙔𝙖𝙚𝙢𝙤𝙧𝙞𝘽𝙤𝙩-𝙈𝘿 ~ 𝘿𝙚𝙫𝘿𝙞𝙚𝙜𝙤

--- plugins/Rg•setgenre.js
+++ plugins/Rg•setgenre.js
@@ -3,12 +3,17 @@ import fetch from 'node-fetch';
 
 const handler = async (m, { conn, command, usedPrefix, text }) => {
 
-if (!text) return conn.reply(m.chat, `✧ Debes ingresar un genero valido.\n> Ejemplo » *${usedPrefix + command} hombre*`, m);
+if (!text) return conn.reply(m.chat, `✧ Debes ingresar un genero válido.\n> Ejemplo » *${usedPrefix + command} hombre*`, m);
+
+let user = global.db.data.users[m.sender];
+
+if (user.genre) {
+return conn.reply(m.chat, `✧ Ya tienes un genero establecido: *${user.genre}*. Si deseas cambiarlo, por favor borra tu género actual primero con el comando *${usedPrefix}delgenero*`, m);
+}
 
 function asignarGenre(text) {
-if (text == 0 || text > 3) return conn.reply(m.chat, `✧ Debes ingresar un genero valido.\n> Ejemplo » *${usedPrefix + command} hombre*`, m);
 let genre;
-switch (text) {
+switch (text.toLowerCase()) {
 case "hombre":
 genre = "Hombre";
 break;
@@ -16,21 +21,21 @@ case "mujer":
 genre = "Mujer";
 break;
 default:
-return conn.reply(m.chat, `✦ Recuerda elegir el genero.\n> Ejemplo: ${usedPrefix + command} hombre`, m);
+return conn.reply(m.chat, `✦ Recuerda elegir un género válido.\n> Ejemplo: ${usedPrefix + command} hombre`, m);
 }
 return genre;
-    }
+}
+
 let genre = asignarGenre(text);
 if (!genre) return;
 
-let user = global.db.data.users[m.sender];
 user.genre = genre;
 
 if (user.genre) {
-return conn.reply(m.chat, `✐ Se ha establecido tu genero como: *${user.genre}*!`, m)
+return conn.reply(m.chat, `✐ Se ha establecido tu genero como: *${user.genre}*!`, m);
 }};
 
-handler.help = ['setgenre']
+handler.help = ['setgenero']
 handler.tags = ['rg']
 handler.command = ['setgenero', 'setgenre']
 export default handler;
