302cc312c78f80ce06c46a845ef99a0dcdc64abb
꧁☆*κɪɴɢ*☆꧂ <argumedodie@gmail.com>
2024-12-02 14:32:15 -0500

    𝙔𝙖𝙚𝙢𝙤𝙧𝙞𝘽𝙤𝙩-𝙈𝘿 ~ 𝘿𝙚𝙫𝘿𝙞𝙚𝙜𝙤

--- plugins/Rg•births.js
+++ plugins/Rg•births.js
@@ -4,7 +4,6 @@ import fetch from 'node-fetch';
 const handler = async (m, { conn }) => {
 let users = global.db.data.users;
 let nacimientos = [];
-let mentions = [];
 
 for (let userId in users) {
 let user = users[userId];
@@ -19,7 +18,7 @@ return conn.reply(m.chat, `✧ No hay fechas de nacimiento registradas en este g
 }
 
 let mensaje = `✐ Fechas de nacimiento en el grupo:\n\n${nacimientos.join('\n')}`;
-return conn.reply(m.chat, mensaje, m, { mentions });
+return conn.reply(m.chat, mensaje, m, { mentions: [userId] });
 };
 
 handler.help = ['births']
