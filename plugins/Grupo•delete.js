f1d767189df37d802f5e9be7b5c637429fe3d33f
꧁☆*κɪɴɢ*☆꧂ <argumedodie@gmail.com>
2024-12-02 16:53:42 -0500

    𝙔𝙖𝙚𝙢𝙤𝙧𝙞𝘽𝙤𝙩-𝙈𝘿 ~ 𝘿𝙚𝙫𝘿𝙞𝙚𝙜𝙤

--- plugins/Grupo•delete.js
+++ plugins/Grupo•delete.js
@@ -1,15 +1,21 @@
+import { createHash } from 'crypto';  
+import fetch from 'node-fetch';
+
 const handler = async (m, { conn, usedPrefix, command }) => {
 
-if (!m.quoted) return conn.reply(m.chat, `🚩 Responde al mensaje que deseas eliminar.`, m, rcanal);
+if (!m.quoted) return conn.reply(m.chat, `🚩 Responde al mensaje que deseas eliminar.`, m);
 
 try {
 let delet = m.message.extendedTextMessage.contextInfo.participant;
 let bang = m.message.extendedTextMessage.contextInfo.stanzaId;
 
-await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }}, { quoted: false });
-} catch {
-await conn.sendMessage(m.chat, { delete: m.quoted.vM.key }, { quoted: false });
-}};
+await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet } }, { quoted: null });
+} catch (e) {
+try {
+await conn.sendMessage(m.chat, { delete: m.quoted.vM.key }, { quoted: null });
+} catch (err) {
+return conn.reply(m.chat, `❌ Error al eliminar el mensaje.`, m);
+}}};
 
 handler.help = ['delete'];
 handler.tags = ['grupo'];
