b7a89d7d4af71f5e2f0d483481ee29bbe36e88b1
꧁☆*κɪɴɢ*☆꧂ <argumedodie@gmail.com>
2024-12-02 14:09:05 -0500

    𝙔𝙖𝙚𝙢𝙤𝙧𝙞𝘽𝙤𝙩-𝙈𝘿 ~ 𝘿𝙚𝙫𝘿𝙞𝙚𝙜𝙤

--- plugins/Rg•setbirth.js
+++ plugins/Rg•setbirth.js
@@ -3,14 +3,12 @@ import fetch from 'node-fetch';
 
 const handler = async (m, { conn, command, usedPrefix, text }) => {
 
-if (!text) return conn.reply(m.chat, `《✧》Debes ingresar una fecha valida para tu cumpleaños.\n\n> ✐ Ejemplo 1 » *${usedPrefix + command} 01/01/2000* (mes/dia/año)\n> ✐ Ejemplo 2 » *${usedPrefix + command} 01/01* (mes/dia)\n> ✐ Ejemplo 3 » *${usedPrefix + command} 1 january*\n> ✐ Ejemplo 4 » *${usedPrefix + command} 24 december*`, m);
+if (!text) return conn.reply(m.chat, `《✧》Debes ingresar una fecha valida para tu cumpleaños.\n\n> ✐ Ejemplo » *${usedPrefix + command} 01/01/2000* (mes/dia/año)\n> ✐ Ejemplo 2 » *${usedPrefix + command} 01/01* (mes/dia)`, m);
 
 function validarFechaNacimiento(text) {
 const opcionesFecha = [
 /^\d{1,2}\/\d{1,2}\/\d{4}$/, // dd/mm/yyyy or m/d/yyyy
 /^\d{1,2}\/\d{1,2}$/, // dd/mm or m/d
-/^\d{1,2} [a-z]+ \d{4}$/i, // 1 diciembre 2024
-/^\d{1,2} de [a-z]+$/i // 1 de junio
 ];
 
 let esValida = opcionesFecha.some(regex => regex.test(text));
