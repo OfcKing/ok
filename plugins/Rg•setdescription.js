c1d8b88a215680fb2f4f207fed68a0433c4db1ab
꧁☆*κɪɴɢ*☆꧂ <argumedodie@gmail.com>
2024-12-02 15:02:41 -0500

    𝙔𝙖𝙚𝙢𝙤𝙧𝙞𝘽𝙤𝙩-𝙈𝘿 ~ 𝘿𝙚𝙫𝘿𝙞𝙚𝙜𝙤

--- plugins/Rg•setdescription.js
+++ plugins/Rg•setdescription.js
@@ -3,13 +3,13 @@ import fetch from 'node-fetch';
 
 const handler = async (m, { conn, command, usedPrefix, text }) => {
 
-if (!text) return conn.reply(m.chat, `♲︎ Debes especificar una descripcion valida para tu perfil.\n\n> ✐ Ejemplo » *${usedPrefix + command} Hola, uso WhatsApp!*`, m);
-
 let user = global.db.data.users[m.sender];
 
 if (user.description) {
 return conn.reply(m.chat, `✧ Ya tienes una descripción establecida, si quieres borrar la descripcion actual usa:\n> » ${usedPrefix}deldescription`, m);
 }
+
+if (!text) return conn.reply(m.chat, `♲︎ Debes especificar una descripcion valida para tu perfil.\n\n> ✐ Ejemplo » *${usedPrefix + command} Hola, uso WhatsApp!*`, m);
 
 user.description = text;
 
