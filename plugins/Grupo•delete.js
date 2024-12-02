27a971d60d408253c62b716aa9a87575d946e3c4
꧁☆*κɪɴɢ*☆꧂ <argumedodie@gmail.com>
2024-12-02 16:51:45 -0500

    𝙔𝙖𝙚𝙢𝙤𝙧𝙞𝘽𝙤𝙩-𝙈𝘿 ~ 𝘿𝙚𝙫𝘿𝙞𝙚𝙜𝙤

--- plugins/Grupo•delete.js
+++ plugins/Grupo•delete.js
@@ -1,19 +1,21 @@
-let handler = async (m, { conn, usedPrefix, command }) => {
+const handler = async (m, { conn, usedPrefix, command }) => {
 
-if (!m.quoted) return conn.reply(m.chat, `🚩 Responde al mensaje que deseas eliminar.`, m, rcanal)
+if (!m.quoted) return conn.reply(m.chat, `🚩 Responde al mensaje que deseas eliminar.`, m, rcanal);
+
 try {
-let delet = m.message.extendedTextMessage.contextInfo.participant
-let bang = m.message.extendedTextMessage.contextInfo.stanzaId
-return conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})
+let delet = m.message.extendedTextMessage.contextInfo.participant;
+let bang = m.message.extendedTextMessage.contextInfo.stanzaId;
+
+await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }}, { quoted: false });
 } catch {
-return conn.sendMessage(m.chat, { delete: m.quoted.vM.key })
-}}
+await conn.sendMessage(m.chat, { delete: m.quoted.vM.key }, { quoted: false });
+}};
 
-handler.help = ['delete']
-handler.tags = ['grupo']
-handler.command = ['delete', 'del']
-handler.group = false
-handler.admin = true
-handler.botAdmin = true
+handler.help = ['delete'];
+handler.tags = ['grupo'];
+handler.command = ['delete', 'del'];
+handler.group = false;
+handler.admin = true;
+handler.botAdmin = true;
 
-export default handler
+export default handler;
