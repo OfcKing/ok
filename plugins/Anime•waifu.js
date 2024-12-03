7a50a03397f4551a9bd9898ebde0c8cd71a381bd
YaemoriBot-MD <theyaemoribot@gmail.com>
2024-12-02 21:33:17 -0500

    𝙔𝙖𝙚𝙢𝙤𝙧𝙞𝘽𝙤𝙩-𝙈𝘿 ~ 𝘿𝙚𝙫𝘿𝙞𝙚𝙜𝙤

--- plugins/Anime•waifu.js
+++ plugins/Anime•waifu.js
@@ -4,10 +4,10 @@ let handler = async (m, { conn, usedPrefix, command }) => {
 try {
 let res = await fetch('https://deliriussapi-oficial.vercel.app/anime/waifu')
 if (!res.ok) return
-let json = await res.json()
-const result = json.data;
-if (!json.url) return 
-await conn.sendFile(m.chat, json.url, 'thumbnail.jpg', '✧ Nombre » ${result.title}\n✐ Tamaño » ${result.size}\n✿ Publicado » ${result.upload}\n♲︎ Likes » ${result.likes}', m)
+let result = await res.json()
+//const result = json.data;
+if (!result.image) return 
+await conn.sendFile(m.chat, json.image, 'thumbnail.jpg', '✧ Nombre » ${result.title}\n✐ Tamaño » ${result.size}\n✿ Publicado » ${result.upload}\n♲︎ Likes » ${result.likes}', m)
 } catch {
 }}
 handler.help = ['waifu']
