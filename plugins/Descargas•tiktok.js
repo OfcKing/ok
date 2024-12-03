a3f8a5a485af1f7610bb853a7e23268aa4187976
YaemoriBot-MD <theyaemoribot@gmail.com>
2024-12-03 10:37:28 -0500

    𝙔𝙖𝙚𝙢𝙤𝙧𝙞𝘽𝙤𝙩-𝙈𝘿 ~ 𝘿𝙚𝙫𝘿𝙞𝙚𝙜𝙤

--- plugins/Descargas•tiktok.js
+++ plugins/Descargas•tiktok.js
@@ -11,7 +11,7 @@ const handler = async (m, { conn, text }) => {
 
     if (!json || !json.url) return conn.reply(m.chat, '✐ No se pudo descargar el video. Verifica que la URL sea correcta.', m);
 
-    await conn.sendMessage(m.chat, { video: { url: json.org }, caption: `✐ Likes » ${json.like}\n✧ Comentarios » ${json.comment}\n♲︎ Author » ${json.username}` }, { quoted: m });
+    await conn.sendMessage(m.chat, { video: { url: json.wm }, caption: `✐ Likes » ${json.like}\n✧ Comentarios » ${json.comment}\n♲︎ Author » ${json.username}` }, { quoted: m });
 
   } catch (e) {
     conn.reply(m.chat, '✐ Ocurrió un error al descargar el video.', m);
