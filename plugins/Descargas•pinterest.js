4d508e63df8a194b1edfa8a85d4e58b0ce9ff612
YaemoriBot-MD <theyaemoribot@gmail.com>
2024-12-02 20:31:25 -0500

    𝙔𝙖𝙚𝙢𝙤𝙧𝙞𝘽𝙤𝙩-𝙈𝘿 ~ 𝘿𝙚𝙫𝘿𝙞𝙚𝙜𝙤

--- plugins/Descargas•pinterest.js
+++ plugins/Descargas•pinterest.js
@@ -13,7 +13,7 @@ let handler = async (m, { conn, text }) => {
 
     const result = json.data[Math.floor(Math.random() * json.data.length)];
     
-    let message = `❀ Usuario » ${result.user}\n❏ Tablero » ${result.board}\n🜸 Link » ${result.url}`;
+    let message = `❀ Usuario » ${creador}\n❏ Estado » ${verdadero}\n🜸 Link » ${imagen}\n✧ Creado en » ${creado_en}`;
     await conn.sendMessage(m.chat, { image: { url: result.image }, caption: message }, { quoted: m });
 
   } catch (e) {
