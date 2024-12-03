809a078236083aec477962479b038f303c611595
YaemoriBot-MD <theyaemoribot@gmail.com>
2024-12-03 09:35:51 -0500

    𝙔𝙖𝙚𝙢𝙤𝙧𝙞𝘽𝙤𝙩-𝙈𝘿 ~ 𝘿𝙚𝙫𝘿𝙞𝙚𝙜𝙤

--- plugins/Descargas•facebook.js
+++ plugins/Descargas•facebook.js
@@ -1,17 +1,17 @@
-import fetch from 'node-fetch';
+import { igdl } from 'ruhend-scraper';
 
 let handler = async (m, { conn, text }) => {
   if (!text) return conn.reply(m.chat, `❏ Ingresa la URL del video de Facebook.`, m);
-
+  
   const facebookAPI = `https://deliriussapi-oficial.vercel.app/download/facebook?url=${text}`;
 
   try {
-    const res = await fetch(facebookAPI);
-    const json = await res.json();
+    const res = await axios.get(facebookAPI);
+    const json = res.data;
 
     if (!json || !json.url) return conn.reply(m.chat, `✧ No se pudo descargar el video. Verifica que la URL sea correcta.`, m);
 
-    await conn.sendMessage(m.chat, { video: { url: json.sd }, caption: `✐ Aquí tienes tu video descargado de Facebook.` }, { quoted: m });
+    await conn.sendMessage(m.chat, { video: { url: json.hd }, caption: `✐ Aquí tienes tu video descargado de Facebook.` }, { quoted: m });
 
   } catch (e) {
     conn.reply(m.chat, `✧ Ocurrió un error al descargar el video.`, m);
