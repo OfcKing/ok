f112464fbf1345080bdd9e5c1c3b5ff382d33f2d
꧁☆*κɪɴɢ*☆꧂ <argumedodie@gmail.com>
2024-12-03 14:53:06 -0500

    𝙔𝙖𝙚𝙢𝙤𝙧𝙞𝘽𝙤𝙩-𝙈𝘿 ~ 𝘿𝙚𝙫𝘿𝙞𝙚𝙜𝙤

--- plugins/Rg•perfil.js
+++ plugins/Rg•perfil.js
@@ -30,7 +30,7 @@ let handler = async (m, { conn }) => {
   await conn.sendMessage(m.chat, { 
     text: profileText,
     contextInfo: {
-      mentionedJid: mentionedJid,
+      mentionedJid: [userId, user.marry],
       externalAdReply: {
         title: '✧ Perfil de Usuario ✧',
         body: packname,
