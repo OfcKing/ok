7d47a8eeee4cc2d679165653a637ff214891f4d7
YaemoriBot-MD <theyaemoribot@gmail.com>
2024-12-03 08:59:29 -0500

    𝙔𝙖𝙚𝙢𝙤𝙧𝙞𝘽𝙤𝙩-𝙈𝘿 ~ 𝘿𝙚𝙫𝘿𝙞𝙚𝙜𝙤

--- plugins/Descargas•facebook.js
+++ plugins/Descargas•facebook.js
@@ -1,47 +1,28 @@
-import { igdl } from 'ruhend-scraper'
+import axios from 'axios';
 
-const handler = async (m, { text, conn, args, usedPrefix, command }) => {
-if (!args[0]) {
-return conn.reply(m.chat, '🚩 Ingresa Un Link De Facebook', m, rcanal)}
-let res
-try {
-await m.react(rwait)
-conn.reply(m.chat, `🕒 *Descargando su video de facebook.*`, m, {
-contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
-title: packname,
-body: dev,
-previewType: 0, 
-thumbnail: icons,
-sourceUrl: channel }}})
-res = await igdl(args[0])
-} catch {
-await m.react(error)
-return conn.reply(m.chat, '🚩 Error al obtener datos. Verifica el enlace.', m, fake)}
-let result = res.data
-if (!result || result.length === 0) {
-return conn.reply(m.chat, '🚩 No se encontraron resultados.', m, fake)}
-let data
-try {
-await m.react(rwait)
-data = result.find(i => i.resolution === "720p (HD)") || result.find(i => i.resolution === "360p (SD)")
-} catch {
-await m.react(error)
-return conn.reply(m.chat, '🚩 Error al procesar los datos.', m, rcanal)}
-if (!data) {
-return conn.reply(m.chat, '🚩 No se encontró una resolución adecuada.', m, rcanal)}
-let video = data.url
-try {
-await m.react(rwait)
-await conn.sendMessage(m.chat, { video: { url: video }, caption: '🚩 *Video de Facebook*\n' + textbot, fileName: 'fb.mp4', mimetype: 'video/mp4' }, { quoted: m })
-await m.react(done)
-} catch {
-await m.react(error)
-return conn.reply(m.chat, '🚩 Error al enviar el video.', m, rcanal)}}
+let handler = async (m, { conn, text }) => {
+  if (!text) return conn.reply(m.chat, `❏ Ingresa la URL del video de Facebook.`, m);
+  
+  const facebookAPI = `https://deliriussapi-oficial.vercel.app/api/facebook?url=${text}`;
 
-handler.help = ['facebook', 'fb']
-handler.tags = ['descargas']
-handler.command = ['facebook', 'fb']
-handler.chocolates = 1
-handler.register = true
+  try {
+    const res = await axios.get(facebookAPI);
+    const json = res.data;
 
-export default handler
+    if (!json || !json.url) return conn.reply(m.chat, `✧ No se pudo descargar el video. Verifica que la URL sea correcta.`, m);
+
+    await conn.sendMessage(m.chat, { video: { url: json.url }, caption: `✐ Aquí tienes tu video descargado de Facebook.` }, { quoted: m });
+
+  } catch (e) {
+    conn.reply(m.chat, `✧ Ocurrió un error al descargar el video.`, m);
+    console.log(e);
+  }
+};
+
+handler.help = ['facebook', 'fb'];
+handler.tags = ['descargas'];
+handler.command = ['facebook', 'fb'];
+handler.chocolates = 1;
+handler.register = true;
+
+export default handler;
