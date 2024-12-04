a3425305aaebbc36018d1c451e3e461d14981091
꧁☆*κɪɴɢ*☆꧂ <argumedodie@gmail.com>
2024-12-04 08:34:34 -0500

    𝙔𝙖𝙚𝙢𝙤𝙧𝙞𝘽𝙤𝙩-𝙈𝘿 ~ 𝘿𝙚𝙫𝘿𝙞𝙚𝙜𝙤

--- plugins/Main•menu.js
+++ plugins/Main•menu.js
@@ -11,83 +11,66 @@ let perfil = await conn.profilePictureUrl(m.sender, 'image').catch(_ => 'https:/
 
 ✧ → ᴘᴀʀᴀ ᴄʀᴇᴀʀ ᴜɴ sᴜʙ-ʙᴏᴛ ᴄᴏɴ ᴛᴜ ɴᴜᴍᴇʀᴏ ᴜᴛɪʟɪᴢᴀ *#serbot* o *#serbot code*
 
-✧ *Menú de Comandos* ✧
+»  ⊹˚• *Tools* ⊹
 
 ✐ *#s • #sticker*
 > ➣ Realiza un sticker.
-
 ✐ *#wm*
 > ➣ Cambiar el nombre del sticker.
+✐ *#tourl*
+> ➣ Convierte imagen en url.
+
+»  ⊹˚• *Sockets* ⊹
 
 ✐ *#token • #gettoken*
 > ➣ Obtén el token del socket.
-
 ✐ *#socket • #bots*
 > ➣ Ver todos loa sockets activos.
-
 ✐ *#serbot • #serbot code • #serbot --code*
 > ➣ Convierte en un socket.
 
+»  ⊹˚• *Rg* ⊹
+
 ✐ *#comprarpremium*
 > ➣ Usar el bot sin limíte
-
 ✐ *#reg • #verificar • #register*
 > ➣ Registra tu nombre y edad en el bot.
-
 ✐ *#unreg*
 > ➣ Elimina tu registro del bot.
-
 ✐ *#setgenre • #setgenero*
 > ➣ Establece tu género en el perfil del bot.
-
 ✐ *#delgenre • #delgenero*
 > ➣ Elimina tu género del perfil del bot.
-
 ✐ *#setbirth • #setnacimiento*
 > ➣ Establece tu fecha de nacimiento en el perfil del bot.
-
 ✐ *#delbirth • #delnacimiento*
 > ➣ Elimina tu fecha de nacimiento del perfil del bot.
-
 ✐ *#setdescription • #setdesc*
 > ➣ Establece una descripción en tu perfil del bot.
-
 ✐ *#deldescription • #deldesc*
 > ➣ Elimina la descripción de tu perfil del bot.
-
 ✐ *#profile*
 > ➣ Muestra tu perfil de usuario.
-
-✐ *#setname*
-> ➣ Cambia el name del Bot.
-
-✐ *#setbanner*
-> ➣ Cambia la imagen del menú del Bot.
-
 ✐ *#marry*
 > ➣ Propón matrimonio a otro usuario.
+
+»  ⊹˚• *Grupo* ⊹
 
 ✐ *#hidetag*
 > ➣ Envia un mensaje mencionando a todos los usuarios
 
-✐ *#tourl*
-> ➣ Convierte imagen en url.
+»  ⊹˚• *Descargas* ⊹
 
 ✐ *#tiktok • #tt*
 > ➣ Descarga videos de TikTok.
-
 ✐ *#pinterest*
 > ➣ Busca y descarga imágenes de Pinterest.
-
 ✐ *#play •#play2*
 > ➣ Descarga música/video de YouTube.
-
 ✐ *#fb • #facebook*
 > ➣ Descarga videos de Facebook.
-
 ✐ *#ig • #instagram*
 > ➣ Descarga contenido de Instagram.
-
 ✐ *#imagen*
 > ➣ Busca y descarga imágenes desde Internet.
   `.trim();
