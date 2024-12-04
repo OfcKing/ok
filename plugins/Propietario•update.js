89db4676ed6ee5bedda47763c91fc7ee504b1a19
꧁☆*κɪɴɢ*☆꧂ <argumedodie@gmail.com>
2024-12-04 08:14:01 -0500

    𝙔𝙖𝙚𝙢𝙤𝙧𝙞𝘽𝙤𝙩-𝙈𝘿 ~ 𝘿𝙚𝙫𝘿𝙞𝙚𝙜𝙤

--- plugins/Propietario•update.js
+++ plugins/Propietario•update.js
@@ -1,22 +1,22 @@
 import { exec } from 'child_process';
 
 let handler = async (m, { conn }) => {
-
   m.reply('✐ Actualizando el bot...');
+
   exec('git pull', (err, stdout, stderr) => {
     if (err) {
-      return conn.reply(m.chat, `✐ Error: ${err.message}`, m);
+      return conn.reply(m.chat, `✐ Error: No se pudo realizar la actualización.\nRazón: ${err.message}`, m);
     }
     if (stderr) {
-      return conn.reply(m.chat, `✐ Stderr: ${stderr}`, m);
+      return conn.reply(m.chat, `✐ Error: Se produjo un problema durante la actualización.\nDetalles: ${stderr}`, m);
     }
-    conn.reply(m.chat, `✐ Stdout: ${stdout}`, m);
+    conn.reply(m.chat, `✐ Update: ${stdout}`, m);
   });
 };
 
 handler.help = ['update'];
 handler.tags = ['owner'];
 handler.command = ['update'];
-handler.rowner = true; 
+handler.rowner = true;  
 
 export default handler;
