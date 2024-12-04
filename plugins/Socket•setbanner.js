3e17ad293d27a32c55308f20344e9e0bd5598151
꧁☆*κɪɴɢ*☆꧂ <argumedodie@gmail.com>
2024-12-03 20:48:51 -0500

    𝙔𝙖𝙚𝙢𝙤𝙧𝙞𝘽𝙤𝙩-𝙈𝘿 ~ 𝘿𝙚𝙫𝘿𝙞𝙚𝙜𝙤

--- plugins/Socket•setbanner.js
+++ plugins/Socket•setbanner.js
@@ -2,43 +2,42 @@ import fs from 'fs';
 import path from 'path';  
 
 let handler = async (m, { conn, isRowner }) => {
+
+let time = global.db.data.users[m.sender].lastmiming + 60000
+if (new Date - global.db.data.users[m.sender].lastmiming < 60000) return conn.reply(m.chat, `✐ Debes esperar ${msToTime(time - new Date())} para poder cambiar la foto del bot.`, m);
+
   try {
-    if (!isSubBot()) {
-      return m.reply('Este comando solo es funcional en subbots.');
-    }
 
     const media = await m.quoted.download();
 
     if (!isImageValid(media)) {
-      return m.reply('El archivo enviado no es una imagen válida.');
+      return m.reply('✧ El archivo enviado no es una imagen válida.');
     }
-
     global.imagen1 = media;
     global.imagen2 = media;  
     global.imagen3 = media;
 
-    m.reply('El banner fue actualizado');
+    m.reply('✐ El banner fue actualizado');
 
   } catch (error) {
     console.error(error);
-    m.reply('Hubo un error al intentar cambiar el banner.');
+    m.reply('✧ Hubo un error al intentar cambiar el banner.');
   }
-};
-
-const isSubBot = () => {
-  return global.botName && global.botName.includes('SubBot');
 };
 
 const isImageValid = (buffer) => {
   const magicBytes = buffer.slice(0, 4).toString('hex');
 
+
   if (magicBytes === 'ffd8ffe0' || magicBytes === 'ffd8ffe1' || magicBytes === 'ffd8ffe2') {
     return true;
   }
 
+
   if (magicBytes === '89504e47') {
     return true;
   }
+
 
   if (magicBytes === '47494638') {
     return true;
@@ -50,6 +49,19 @@ const isImageValid = (buffer) => {
 handler.help = ['setbanner'];  
 handler.tags = ['socket'];    
 handler.command = ['setban', 'setbanner'];  
-handler.rowner = true;
+//handler.rowner = true
 
 export default handler;
+
+function msToTime(duration) {
+var milliseconds = parseInt((duration % 1000) / 100),
+seconds = Math.floor((duration / 1000) % 60),
+minutes = Math.floor((duration / (1000 * 60)) % 60),
+hours = Math.floor((duration / (1000 * 60 * 60)) % 24)
+
+hours = (hours < 10) ? '0' + hours : hours
+minutes = (minutes < 10) ? '0' + minutes : minutes
+seconds = (seconds < 10) ? '0' + seconds : seconds
+
+return minutes + ' m y ' + seconds + ' s '
+}
