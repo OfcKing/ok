c5e893521bff07cc437f0d9edbda5e7b2e4fbd2e
꧁☆*κɪɴɢ*☆꧂ <argumedodie@gmail.com>
2024-12-05 13:51:02 -0500

    =͟͟͞𝐘𝐚𝐞𝐦𝐨𝐫𝐢 𝐁𝐨𝐭⏤͟͟͞͞★

--- plugins/Game•abrazar.js
+++ plugins/Game•abrazar.js
@@ -1,30 +1,25 @@
-import fetch from 'node-fetch';
-
-const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, isPrems}) => {
+import { sticker } from '../lib/sticker.js'
+import fetch from 'node-fetch'
+let MessageType = (await import('@whiskeysockets/baileys')).default
+let handler = async (m, { conn}) => {
+try {
 let who
 if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
 else who = m.chat
-if (!who) return conn.reply(m.chat, '🚩 Menciona al usuario con *@user*', m, rcanal);
-if (usedPrefix == 'a' || usedPrefix == 'A') return;
-let url = ["https://telegra.ph/file/4d80ab3a945a8446f0b15.mp4", "https://telegra.ph/file/ef3a13555dfa425fcf8fd.mp4", "https://telegra.ph/file/582e5049e4070dd99a995.mp4", "https://telegra.ph/file/ab57cf916c5169f63faee.mp4", "https://telegra.ph/file/fce96960010f6d7fc1670.mp4", "https://telegra.ph/file/33332f613e1ed024be870.mp4"].getRandom();
-try {
-const locale = 'es-ES';
-let name = conn.getName(m.sender);
-let name2 = conn.getName(who);
-const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
-const document = doc[Math.floor(Math.random() * doc.length)];
-let stiker = await sticker(null, url, `${name} está alimentando a ${name2}`)
-if (m.isGroup) {
-conn.sendFile(m.chat, stiker, null, { asSticker: true });
-} else {
-conn.sendFile(m.chat, stiker, null, { asSticker: true });
-}
-} catch {
-conn.reply(m.chat, '🍟 *¡Ocurrio un error!*', m, fake);
-}};
-
+if (!who) return m.reply(`🚩 Etiqueta o menciona a alguien`)
+if(m.quoted?.sender) m.mentionedJid.push(m.quoted.sender)
+if(!m.mentionedJid.length) m.mentionedJid.push(m.sender)
+let res = await fetch('https://nekos.life/api/v2/img/hug')
+let json = await res.json()
+let name = conn.getName(m.sender)
+let name2 = conn.getName(who)
+if (m.isGroup){
+let { url } = json
+let stiker = await sticker(null, url, `${name} le dió un fuerte abrazo a ${name2}`)
+conn.sendFile(m.chat, stiker, null, { asSticker: true })}
+} catch (e) { }}
 handler.help = ['hug *<@user>*']
-handler.tags = ['fun'];
-handler.command = ['abrazar', 'hug', 'abrazo'];
-handler.register = true;
-export default handler;
+handler.tags = ['fun']
+handler.command = ['abrazar', 'hug']
+handler.group = true
+export default handler
