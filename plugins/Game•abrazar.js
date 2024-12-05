7ff2fc9c0033f363e689cc91b5373045188e076d
꧁☆*κɪɴɢ*☆꧂ <argumedodie@gmail.com>
2024-12-05 13:46:16 -0500

    =͟͟͞𝐘𝐚𝐞𝐦𝐨𝐫𝐢 𝐁𝐨𝐭⏤͟͟͞͞★

--- plugins/Game•abrazar.js
+++ plugins/Game•abrazar.js
@@ -6,29 +6,24 @@ if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted
 else who = m.chat
 if (!who) return conn.reply(m.chat, '🚩 Menciona al usuario con *@user*', m, rcanal);
 if (usedPrefix == 'a' || usedPrefix == 'A') return;
-let pp = "https://telegra.ph/file/4d80ab3a945a8446f0b15.mp4"
-let pp2 = "https://telegra.ph/file/ef3a13555dfa425fcf8fd.mp4"
-let pp3 = "https://telegra.ph/file/582e5049e4070dd99a995.mp4"
-let pp4 = "https://telegra.ph/file/ab57cf916c5169f63faee.mp4"
-let pp5 = "https://telegra.ph/file/fce96960010f6d7fc1670.mp4"
-let pp6 = "https://telegra.ph/file/33332f613e1ed024be870.mp4"
+let url = ["https://telegra.ph/file/4d80ab3a945a8446f0b15.mp4", "https://telegra.ph/file/ef3a13555dfa425fcf8fd.mp4", "https://telegra.ph/file/582e5049e4070dd99a995.mp4", "https://telegra.ph/file/ab57cf916c5169f63faee.mp4", "https://telegra.ph/file/fce96960010f6d7fc1670.mp4", "https://telegra.ph/file/33332f613e1ed024be870.mp4"].getRandom()
 try {
 const locale = 'es-ES';
 const taguser = '@' + m.sender.split('@s.whatsapp.net')[0];
 const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
 const document = doc[Math.floor(Math.random() * doc.length)];
-const str = `${taguser} le esta dando un fuerte abrazo a @${who.split`@`[0]} 🫂`
+let stiker = await sticker(null, url, `${name} está alimentando a ${name2}`)
 if (m.isGroup) {
-conn.sendMessage(m.chat, { video: { url: [pp, pp2, pp3, pp4, pp5, pp6].getRandom() }, gifPlayback: true, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net')}, {quoted: fkontak});
+conn.sendFile(m.chat, stiker, null, { asSticker: true })}
 } else {
-conn.sendMessage(m.chat, { video: { url: [pp, pp2, pp3, pp4, pp5, pp6].getRandom() }, gifPlayback: true, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net')}, {quoted: fkontak});
+conn.sendFile(m.chat, stiker, null, { asSticker: true })}
 }
 } catch {
 conn.reply(m.chat, '🍟 *¡Ocurrio un error!*', m, fake);
 }};
 
-handler.help = ['abrazar'].map((v) => v + ' <@usuario>');
+handler.help = ['hug *<@user>*']
 handler.tags = ['fun'];
-handler.command = ['abrazar'];
+handler.command = ['abrazar', 'hug', 'abrazo'];
 handler.register = true;
 export default handler;
