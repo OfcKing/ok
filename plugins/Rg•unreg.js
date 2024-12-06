b75c4f9a4ed9f26466faacdcf58f2e4f7a93c77c
YaemoriBot-MD <theyaemoribot@gmail.com>
2024-12-06 09:00:04 -0500

    =͟͟͞𝐘𝐚𝐞𝐦𝐨𝐫𝐢 𝐁𝐨𝐭⏤͟͟͞͞★

--- plugins/Rg•unreg.js
+++ plugins/Rg•unreg.js
@@ -3,11 +3,11 @@ import { createHash } from 'crypto';
 let handler = async function (m, { conn, args }) {
   let user = global.db.data.users[m.sender];
 
-  if (!user.registered) return m.reply('✐ Tu no estás registrado.');
+  if (!user.registered) return m.reply('🌻 Tu no estás registrado.');
 
   user.registered = false;
 
-  m.reply('✐ Registro eliminado exitosamente.');
+  m.reply('✨️ Registro eliminado exitosamente.');
 };
 
 handler.help = ['unreg'];
