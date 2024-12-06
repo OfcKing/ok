5714a98cfd5a2c8202cd5224225134599dd852b7
YaemoriBot-MD <theyaemoribot@gmail.com>
2024-12-06 09:00:46 -0500

    =͟͟͞𝐘𝐚𝐞𝐦𝐨𝐫𝐢 𝐁𝐨𝐭⏤͟͟͞͞★

--- plugins/Owner•join.js
+++ plugins/Owner•join.js
@@ -1,20 +1,20 @@
 let linkRegex = /https:\/\/chat\.whatsapp\.com\/([0-9A-Za-z]{20,24})/i;
 
 let handler = async (m, { conn, text, isOwner }) => {
-    if (!text) return m.reply('✧ Debes enviar una invitacion para que *Yaemori* se una al grupo.');
+    if (!text) return m.reply('☁️ Debes enviar una invitacion para que *Yaemori* se una al grupo.');
 
     let [_, code] = text.match(linkRegex) || [];
 
-    if (!code) return m.reply('✐ Enlace de invitación no válido.');
+    if (!code) return m.reply('🌸 Enlace de invitación no válido.');
 
     if (isOwner) {
         await conn.groupAcceptInvite(code)
-            .then(res => m.reply(`✓ Me he unido exitosamente al grupo.`))
+            .then(res => m.reply(`✨️ Me he unido exitosamente al grupo.`))
             .catch(err => m.reply(`✗ Error al unirme al grupo.`));
     } else {
-        let message = `✐ Invitación a un grupo:\n${text}\n\nPor: @${m.sender.split('@')[0]}`;
+        let message = `🥷 Invitación a un grupo:\n${text}\n\nPor: @${m.sender.split('@')[0]}`;
         await conn.sendMessage('5218713339906' + '@s.whatsapp.net', { text: message, mentions: [m.sender] }, { quoted: m });
-        m.reply(`✐ El link del grupo ha sido enviado, gracias por tu invitacion *ฅ^•ﻌ•^ฅ*`);
+        m.reply(`🍄 El link del grupo ha sido enviado, gracias por tu invitacion.`);
     }
 };
 
