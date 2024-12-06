7428993e64b9725492335f881f98347e7a498987
YaemoriBot-MD <theyaemoribot@gmail.com>
2024-12-06 08:59:43 -0500

    =͟͟͞𝐘𝐚𝐞𝐦𝐨𝐫𝐢 𝐁𝐨𝐭⏤͟͟͞͞★

--- plugins/Game•cf.js
+++ plugins/Game•cf.js
@@ -3,24 +3,24 @@ let users = {};
 let handler = async (m, { conn, text, usedPrefix, command }) => {
     let [eleccion, cantidad] = text.split(' ');
     if (!eleccion || !cantidad) {
-        return m.reply(`✐ Por favor, elige cara o cruz y una cantidad de chocolates para apostar.\nEjemplo: *${usedPrefix + command} cara 50*`);
+        return m.reply(`✨️ Por favor, elige cara o cruz y una cantidad de chocolates para apostar.\nEjemplo: *${usedPrefix + command} cara 50*`);
     }
 
     eleccion = eleccion.toLowerCase();
     cantidad = parseInt(cantidad);
     if (eleccion !== 'cara' && eleccion !== 'cruz') {
-        return m.reply(`✐ Elección no válida. Por favor, elige cara o cruz.\nEjemplo: *${usedPrefix + command} cara*`);
+        return m.reply(`🚩 Elección no válida. Por favor, elige cara o cruz.\nEjemplo: *${usedPrefix + command} cara*`);
     }
 
     if (isNaN(cantidad) || cantidad <= 0) {
-        return m.reply(`✐ Cantidad no válida. Por favor, elige una cantidad de chocolates para apostar.\nEjemplo: *${usedPrefix + command} cara 50*`);
+        return m.reply(`🚩 Cantidad no válida. Por favor, elige una cantidad de chocolates para apostar.\nEjemplo: *${usedPrefix + command} cara 50*`);
     }
 
     let userId = m.sender;
     if (!users[userId]) users[userId] = { chocolates: 100 };
     let user = global.db.data.users[m.sender];
     if (user.chocolates < cantidad) {
-        return m.reply(`✐ No tienes suficientes chocolates para apostar. Tienes ${user.chocolates} chocolates.`);
+        return m.reply(`✨️ No tienes suficientes chocolates para apostar. Tienes ${user.chocolates} chocolates.`);
     }
 
     let resultado = Math.random() < 0.5 ? 'cara' : 'cruz';
