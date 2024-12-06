e57fdf3c8749755c09c525a2fd9092cca1147f14
YaemoriBot-MD <theyaemoribot@gmail.com>
2024-12-06 09:08:17 -0500

    =͟͟͞𝐘𝐚𝐞𝐦𝐨𝐫𝐢 𝐁𝐨𝐭⏤͟͟͞͞★

--- plugins/Descargas•instagram.js
+++ plugins/Descargas•instagram.js
@@ -6,11 +6,13 @@ const handler = async (m, { args, conn }) => {
   }
 
   try {
+    await m.react(rwait);
     const res = await igdl(args[0]);
     const data = res.data;
 
     for (let media of data) {
       await conn.sendFile(m.chat, media.url, 'instagram.mp4', '✨️ Video de Instagram.', m);
+    await m.react(done);
     }
   } catch (e) {
     return conn.reply(m.chat, '✖️ Ocurrió un error.', m);
