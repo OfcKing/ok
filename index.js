c6e88e02cb05aea1d54b9d945b2cac7d1f00855d
Senko San <theyaemoribot@gmail.com>
2024-12-15 14:01:02 -0500

    =͟͟͞𝐒𝐞𝐧𝐤𝐨 𝐒𝐚𝐧 𝐔𝐥𝐭𝐫𝐚⏤͟͟͞͞★

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '1'
 import './settings.js'
 import { join, dirname } from 'path';
 //import { createRequire } from 'module';
-import { fileURLToPath } from 'url';
+//import { fileURLToPath } from 'url';
 import { setupMaster, fork } from 'cluster';
 import { watchFile, unwatchFile } from 'fs';
 import cfonts from 'cfonts';
