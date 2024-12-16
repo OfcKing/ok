a14dfe63a5991362d1e6dde23a9d0ce1562963fb
Senko San <theyaemoribot@gmail.com>
2024-12-16 08:11:42 -0500

    =͟͟͞𝐒𝐞𝐧𝐤𝐨 𝐒𝐚𝐧 𝐔𝐥𝐭𝐫𝐚⏤͟͟͞͞★

---
+++ senko/settings.js
@@ -0,0 +1,102 @@
+import { watchFile, unwatchFile } from 'fs' 
+import chalk from 'chalk'
+import { fileURLToPath } from 'url'
+import fs from 'fs'
+import cheerio from 'cheerio'
+import fetch from 'node-fetch'
+import axios from 'axios'
+import moment from 'moment-timezone' 
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+//BETA: Si quiere evitar escribir el número que será bot en la consola, agregué desde aquí entonces:
+//Sólo aplica para opción 2 (ser bot con código de texto de 8 digitos)
+global.botNumberCode = '' //Ejemplo: +573218138672
+global.confirmCode = ''
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.owner = [
+   ['5218713339906', 'Creador 👑', true],
+   ['5218714429807', 'Owner 🍭', true],
+   ['573012482597'],
+   ['50492280729']
+]
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.mods = []
+global.prems = []
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.vs = '1.0.6'
+global.vsJB = '5.0'
+global.nameqr = 'Ai Senko - MD'
+global.namebot = 'sᥱᥒk᥆ sᥲᥒ - mძ ✰'
+global.sessions = 'SanSession'
+global.jadi = 'SanJadiBot'
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.packname = 'ᥲі sᥱᥒk᥆ - mძ ✿'
+global.botname = 'SenkoBot'
+global.wm = 'SenkoBot-MD ❀'
+global.author = 'Made By OfcKing 🜲'
+global.dev = '© ⍴᥆ᥕᥱrᥱძ ᑲᥡ ᥆𝖿ᥴkіᥒg ✌︎'
+global.textbot = 'Senko San ✐'
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.imagen1 = fs.readFileSync('./senko/icons/Menu.jpg')
+global.imagen2 = fs.readFileSync('./senko/icons/Menu2.jpg')
+global.imagen3 = fs.readFileSync('./senko/icons/Menu3.jpg')
+global.catalogo = fs.readFileSync('./senko/icons/catalogo.jpg')
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.gp1 = 'https://chat.whatsapp.com/GkuIuySiMwb4qJGl3UJtcZ'
+global.channel = 'https://whatsapp.com/channel/0029VaQD7LAJP216tu9liI2A'
+global.channel2 = 'https://whatsapp.com/channel/0029VayCRH2Jf05m1wzaBi1Y'
+global.channel3 = 'https://whatsapp.com/channel/0029Va4QjH7DeON0ePwzjS1A'
+global.yt = 'https://www.youtube.com/@OfcDiego'
+global.tiktok = 'https://tiktok.com/@dev_diego'
+global.correo = 'thesenkobot@gmail.com'
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: { orderMessage: { itemCount : -999999, status: 1, surface : 1, message: '❀ sᥙ́⍴ᥱr ᥕһᥲ𝗍sᥲ⍴⍴ ᑲ᥆𝗍 ☄︎', orderTitle: 'Bang', thumbnail: catalogo, sellerJid: '0@s.whatsapp.net'}}}
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.ch = {
+ch1: '120363370415738881@newsletter',
+ch2: '120363263466636910@newsletter',
+ch3: '120363160031023229@newsletter',
+ch4: '120363307382381547@newsletter',
+ch5: '120363301598733462@newsletter',
+ch6: '120363371018732371@newsletter',
+ch7: '120363350554513092@newsletter',
+}
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.cheerio = cheerio
+global.fs = fs
+global.fetch = fetch
+global.axios = axios
+global.moment = moment        
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.multiplier = 69
+global.maxwarn = '3'
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+let file = fileURLToPath(import.meta.url)
+watchFile(file, () => {
+  unwatchFile(file)
+  console.log(chalk.redBright("Update 'senko/settings.js'"))
+  import(`${file}?update=${Date.now()}`)
+})
